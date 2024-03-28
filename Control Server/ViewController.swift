2379b601a41d115d77761a90baa395a10c71ed9e
techrisdev <techrisdev>
2024-03-28 21:26:42 +0100

    Initial Commit

---
+++ Control Server/ViewController.swift
@@ -0,0 +1,23 @@
+// ViewController.swift
+//
+// Created by TeChris on 28.03.24.
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
